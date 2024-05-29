d78297b9adde9e583fd778f415adc7a708723d97
Gevorg Melkumyan <gevorgmelkoumyan@gmail.com>
2024-05-29 18:25:27 +0400

    Made the port 5173 accessible, setup vue for vite.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,15 @@
-import './bootstrap';
+import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
+import App from './App.vue';
+import Home from './views/Home.vue';
+
+const routes = [
+    { path: '/', component: Home },
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+createApp(App).use(router).mount('#app');
